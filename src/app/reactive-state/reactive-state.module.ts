6727741c8cb1b8c995ad54b5c561072d72e9631e
Charlotka R <charlotka.web@gmail.com>
2022-12-07 12:51:04 +0100

    #005 - implement search on candidates list

--- src/app/reactive-state/reactive-state.module.ts
+++ src/app/reactive-state/reactive-state.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveStateRoutingModule } from './reactive-state-routing.module';
+import { CandidateListComponent } from './components/candidate-list/candidate-list.component';
+import { SingleCandidateComponent } from './components/single-candidate/single-candidate.component';
+import { SharedModule } from '../shared/shared.module';
+import { CandidatesService } from './services/candidates.service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CandidateListComponent,
+    SingleCandidateComponent
+  ],
   imports: [
     CommonModule,
-    ReactiveStateRoutingModule
+    ReactiveStateRoutingModule,
+    SharedModule
+  ],
+  providers: [
+    CandidatesService
   ]
 })
 export class ReactiveStateModule { }
