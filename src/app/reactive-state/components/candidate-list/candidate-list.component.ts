565122adb83626dc1ac3071e2c6c53a5d2528e6d
Charlotka R <charlotka.web@gmail.com>
2023-01-01 16:32:47 +0100

    #006 - delete and update method, end of tutorial

--- src/app/reactive-state/components/candidate-list/candidate-list.component.ts
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -67,4 +67,6 @@ export class CandidateListComponent implements OnInit {
     ];
   }
 
+  
+
 }
--- src/app/reactive-state/components/single-candidate/single-candidate.component.ts
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, switchMap, take, tap } from 'rxjs';
 import { Candidate } from '../../models/candidate.model';
 import { CandidatesService } from '../../services/candidates.service';
 
@@ -30,9 +30,26 @@ export class SingleCandidateComponent implements OnInit {
     );
   }
 
-  onHire() {}
 
-  onRefuse() {}
+  onRefuse() {
+    this.candidate$.pipe(
+        take(1),
+        tap(candidate => {
+            this.candidatesService.refuseCandidate(candidate.id);
+            this.onGoBack();
+        })
+    ).subscribe();
+  }
+
+  onHire() {
+    this.candidate$.pipe(
+        take(1),
+        tap(candidate => {
+            this.candidatesService.hireCandidate(candidate.id);
+            this.onGoBack();
+        })
+    ).subscribe();
+  }
 
   onGoBack() {
       this.router.navigateByUrl('/reactive-state/candidates');
